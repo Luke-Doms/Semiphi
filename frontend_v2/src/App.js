96e0992ceec37ba18713fac9481abddb26e69d63
Luke <90083045+Luke-Doms@users.noreply.github.com>
2024-01-27 18:42:44 -0500

    added routes for the main pages and a puzzle nav bar

--- frontend_v2/src/App.js
+++ frontend_v2/src/App.js
@@ -1,17 +1,15 @@
-import Home from './components/Home';
-import Puzzles from './components/Puzzles';
-import Settings from './components/Settings';
 import NavBar from './components/NavBar';
+import MainSpace from './components/MainSpace';
+import PuzzleNav from './components/PuzzleNav';
 import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className='background'>
       <NavBar />
+      <MainSpace />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/puzzles" element={<Puzzles />}/>
-        <Route path="/settings" element={<Settings />}/>
+        <Route path="/puzzles" element={<PuzzleNav />}/>
       </Routes>
     </div>
   );
---
+++ frontend_v2/src/components/PuzzleNav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function PuzzleNav() {
+  return (
+    <div className='puzzle-nav-container'>
+        <nav className='puzzle-navbar'>
+            <span>puzzle one</span>
+            <span>puzzle two</span>
+            <span>puzzle three</span>
+        </nav>
+    </div>
+  )
+}
+
+export default PuzzleNav
