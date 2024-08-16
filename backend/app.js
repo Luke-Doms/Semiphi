08a81659b4fba536657c46e62428ff343de3651b
Luke <90083045+Luke-Doms@users.noreply.github.com>
2024-08-15 20:02:59 -0400

    implimented express to serve the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3005;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../frontend_v2/build')));
+
+app.listen(PORT, (error) => {
+	if (!error) {
+		console.log("server running");
+	} else {
+		console.log("server not running");
+	}
+});
