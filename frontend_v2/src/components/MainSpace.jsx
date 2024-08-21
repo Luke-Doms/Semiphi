dfadb000b938fe0359caff9d106958364a36ab11
Luke <90083045+Luke-Doms@users.noreply.github.com>
2024-08-21 03:43:58 -0400

    changed theme layout and login flow

--- frontend_v2/src/components/MainSpace.jsx
+++ frontend_v2/src/components/MainSpace.jsx
@@ -19,14 +19,16 @@ function MainSpace() {
       return res.json();
     }).then((json) => {
       setLoggedIn(json.loggedIn);
-      console.log(loggedIn);
     });
   })
 
   const handleClick = () => {
-    console.log(loggedIn);
     if (loggedIn) {
-      navigate('/register');
+      fetch('/logout', {
+        method: "GET"
+      }).then(() => {
+        navigate('/');
+      });
     } else {
       navigate('/login');
     }
@@ -43,7 +45,9 @@ function MainSpace() {
               <svg style={{'cursor': 'pointer'}} xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M427.68 351.43C402 320 383.87 304 383.87 217.35 383.87 138 343.35 109.73 310 96c-4.43-1.82-8.6-6-9.95-10.55C294.2 65.54 277.8 48 256 48s-38.21 17.55-44 37.47c-1.35 4.6-5.52 8.71-9.95 10.53-33.39 13.75-73.87 41.92-73.87 121.35C128.13 304 110 320 84.32 351.43 73.68 364.45 83 384 101.61 384h308.88c18.51 0 27.77-19.61 17.19-32.57zM320 384v16a64 64 0 01-128 0v-16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
             </div>
             <div className='login-icon' onClick={() => {handleClick()}}>
-              <svg style={{'cursor': 'pointer'}} xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M344 144c-3.92 52.87-44 96-88 96s-84.15-43.12-88-96c-4-55 35-96 88-96s92 42 88 96z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M256 304c-87 0-175.3 48-191.64 138.6C62.39 453.52 68.57 464 80 464h352c11.44 0 17.62-10.48 15.65-21.4C431.3 352 343 304 256 304z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/></svg>
+              {loggedIn ?
+              <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M304 336v40a40 40 0 01-40 40H104a40 40 0 01-40-40V136a40 40 0 0140-40h152c22.09 0 48 17.91 48 40v40M368 336l80-80-80-80M176 256h256" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg> :
+              <svg style={{'cursor': 'pointer'}} xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M344 144c-3.92 52.87-44 96-88 96s-84.15-43.12-88-96c-4-55 35-96 88-96s92 42 88 96z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M256 304c-87 0-175.3 48-191.64 138.6C62.39 453.52 68.57 464 80 464h352c11.44 0 17.62-10.48 15.65-21.4C431.3 352 343 304 256 304z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/></svg>}
             </div>
         </div>
         <Routes>
