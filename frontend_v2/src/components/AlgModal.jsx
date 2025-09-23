47335344374ca5416b8ad826228c47a44363c222
Luke <90083045+Luke-Doms@users.noreply.github.com>
2025-09-23 17:16:56 -0400

    restructured modals to use createPortal

--- frontend_v2/src/components/AlgModal.jsx
+++ frontend_v2/src/components/AlgModal.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 import AlgPopup from './AlgPopup.jsx';
 
 const AlgModal = (props) => {
-  return (
+  return createPortal(
     <div onClick={() => props.setModal(false)} className='modal'>
       <AlgPopup />
-    </div>
+    </div>,
+    document.body
   )
 }
 
